4b5a66b1f6ababfd08dce863b0c0505606c56432
Diego Marcelo <diegomcsilva191@gmail.com>
2023-06-25 23:53:08 -0300

    feat: create initial structure to project

---
+++ src/pages/memory-game/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react"
+
+const MemoryGame: React.FC = (): ReactElement => {
+  return (
+    <div className="memory-game">
+      Jogo da memoria
+    </div>
+  )
+}
+
+export default MemoryGame;
